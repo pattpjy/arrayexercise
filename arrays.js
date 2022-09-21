09b68a38bb3ff4e3942d864b380248a55dbfb39c
Patt Sookmark <pijiya@sookmark.com>
2022-09-21 10:38:41 -0600

    Make changes with array methods

--- arrays.js
+++ arrays.js
@@ -2,3 +2,14 @@ var names = ['donut','ice cream','coffee','tea'];
 var codes = [1,2,3,4];
 var stocks = [true,false,true,true];
 console.log(stocks);
+
+// pop remove the last element in the names array
+names.pop();
+// push add lenght to array
+codes.push(5);
+//shift removes the first element from an array and returns that removed element.
+stocks.shift();
+
+console.log(names);
+console.log(codes);
+console.log(stocks);
